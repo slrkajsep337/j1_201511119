514ac66807782d88a74ab7b4127644d4551a31fe
slrkajsep337 <slrkajsep337@naver.com>
2016-04-18 18:58:49 +0900

    pizza

---
+++ src/com/j1/w7/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.j1.w7;
+
+public class ThinCrustDough implements Dough {
+    public String toString() {
+        return "Thin Crust Dough";
+    }
+}
---
+++ src/com/j1/w7/PizzaIngredientFactory.java
@@ -0,0 +1,6 @@
+package com.j1.w7;
+
+public interface PizzaIngredientFactory {
+    public Dough createDough();
+    public Cheese createCheese();
+}
